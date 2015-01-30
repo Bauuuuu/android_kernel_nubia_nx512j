c7dc95abe71617d13dfd660963f7216869551d9a
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2015-01-30 11:52:37 +0530

    sched: Keep track of average nr_big_tasks
    Extend sched_get_nr_running_avg() API to return average nr_big_tasks,
    in addition to average nr_running and average nr_io_wait tasks. Also
    add a new trace point to record values returned by
    sched_get_nr_running_avg() API.
    
    Change-Id: Id3591e6d04da8db484b4d1cb9d95dba075f5ab9a
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>
    [rameezmustafa@codeaurora.org: Resolve trivial merge conflicts]
    Signed-off-by: Syed Rameez Mustafa <rameezmustafa@codeaurora.org>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -749,7 +749,23 @@ u64 scale_load_to_cpu(u64 load, int cpu);
 unsigned int max_task_load(void);
 extern void sched_account_irqtime(int cpu, struct task_struct *curr,
 				 u64 delta, u64 wallclock);
-extern int power_delta_exceeded(unsigned int cpu_cost, unsigned int base_cost);
+
+extern unsigned int nr_eligible_big_tasks(int cpu);
+
+/*
+ * 'load' is in reference to "best cpu" at its best frequency.
+ * Scale that in reference to a given cpu, accounting for how bad it is
+ * in reference to "best cpu".
+ */
+static inline u64 scale_load_to_cpu(u64 task_load, int cpu)
+{
+	struct rq *rq = cpu_rq(cpu);
+
+	task_load *= (u64)rq->load_scale_factor;
+	task_load /= 1024;
+
+	return task_load;
+}
 
 static inline void
 inc_cumulative_runnable_avg(struct rq *rq, struct task_struct *p)
@@ -805,6 +821,11 @@ static inline unsigned long capacity_scale_cpu_freq(int cpu)
 static inline void sched_account_irqtime(int cpu, struct task_struct *curr,
 				 u64 delta, u64 wallclock)
 {
+}
+
+static inline unsigned int nr_eligible_big_tasks(int cpu)
+{
+	return 0;
 }
 
 #endif	/* CONFIG_SCHED_HMP */
