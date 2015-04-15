ca134b04409b3ef4933091631a289a0129bc653e
Theodore Ts'o <tytso@mit.edu>
2015-04-14 20:51:49 -0400

    ext4 crypto: teach ext4_htree_store_dirent() to store decrypted filenames
    For encrypted directories, we need to pass in a separate parameter for
    the decrypted filename, since the directory entry contains the
    encrypted filename.
    
    Change-Id: I7290eb0979d9e9d9d65ee07bbf11223b3382394f
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Signed-off-by: Theodore Ts'o <tytso@google.com>
    
    (cherry picked from commit b02648af2cda5a10cd278f2f4ac16f80679b8934)
    (cherry picked from commit 66cf5ca0d92f71e4a54ef3998e3312c634aeaaea)

--- fs/ext4/inline.c
+++ fs/ext4/inline.c
@@ -1310,6 +1310,7 @@ int htree_inlinedir_to_tree(struct file *dir_file,
 	struct ext4_iloc iloc;
 	void *dir_buf = NULL;
 	struct ext4_dir_entry_2 fake;
+	struct ext4_str tmp_str;
 
 	ret = ext4_get_inode_loc(inode, &iloc);
 	if (ret)
@@ -1381,8 +1382,10 @@ int htree_inlinedir_to_tree(struct file *dir_file,
 			continue;
 		if (de->inode == 0)
 			continue;
-		err = ext4_htree_store_dirent(dir_file,
-				   hinfo->hash, hinfo->minor_hash, de);
+		tmp_str.name = de->name;
+		tmp_str.len = de->name_len;
+		err = ext4_htree_store_dirent(dir_file, hinfo->hash,
+					      hinfo->minor_hash, de, &tmp_str);
 		if (err) {
 			count = err;
 			goto out;
