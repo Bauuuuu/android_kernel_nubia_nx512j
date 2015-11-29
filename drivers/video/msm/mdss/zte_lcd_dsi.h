c8e7b9e8dcd3086d25a24d897525c9e046c41d75
faust93 <monumentum@gmail.com>
2015-11-29 17:59:16 +0300

    fix LiveDisplay

--- drivers/video/msm/mdss/zte_lcd_dsi.h
+++ drivers/video/msm/mdss/zte_lcd_dsi.h
@@ -2,5 +2,8 @@
 #define ZTE_LCD_DSI_H
 
 int zte_dsi_panel_reg_read(struct mdss_dsi_ctrl_pdata *ctrl, u8 reg, u8 *rbuf, u8 len);
+/* EXTERN FUNCTION */
+extern void mdss_dsi_panel_cmds_send(struct mdss_dsi_ctrl_pdata *ctrl,
+			struct dsi_panel_cmds *pcmds);
 
 #endif //ZTE_LCD_DSI_H
