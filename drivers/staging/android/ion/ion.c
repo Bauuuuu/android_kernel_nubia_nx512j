a0f538f6b32b7f5de902d742d686a8c788121a8e
Neil Zhang <neilzhang1123@hotmail.com>
2016-01-26 17:39:06 +0800

    BACKPORT: ion:synchronize debugfs callback and ion_client_destroy
    (cherry picked from commit 948c4db4ee10d85fe78ed3755dcaeb85cd37a148)
    
    There are race condition B/T ion_client_destroy and debugfs callbacks.
    Let's use a mutex to synchronize them.
    
    Change-Id: I4d73b258ac1fb67604b62f237da0e004275fd3da
    Signed-off-by: Neil Zhang <neilzhang1123@hotmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -785,6 +785,34 @@ void ion_unmap_kernel(struct ion_client *client, struct ion_handle *handle)
 }
 EXPORT_SYMBOL(ion_unmap_kernel);
 
+static struct mutex debugfs_mutex;
+static struct rb_root *ion_root_client;
+static int is_client_alive(struct ion_client *client)
+{
+	struct rb_node *node;
+	struct ion_client *tmp;
+	struct ion_device *dev;
+
+	node = ion_root_client->rb_node;
+	dev = container_of(ion_root_client, struct ion_device, clients);
+
+	down_read(&dev->lock);
+	while (node) {
+		tmp = rb_entry(node, struct ion_client, node);
+		if (client < tmp) {
+			node = node->rb_left;
+		} else if (client > tmp) {
+			node = node->rb_right;
+		} else {
+			up_read(&dev->lock);
+			return 1;
+		}
+	}
+
+	up_read(&dev->lock);
+	return 0;
+}
+
 static int ion_debug_client_show(struct seq_file *s, void *unused)
 {
 	struct ion_client *client = s->private;
@@ -817,6 +845,14 @@ static int ion_debug_client_show(struct seq_file *s, void *unused)
 			"heap_name", "size_in_bytes", "handle refcount",
 			"buffer");
 
+	mutex_lock(&debugfs_mutex);
+	if (!is_client_alive(client)) {
+		seq_printf(s, "ion_client 0x%p dead, can't dump its buffers\n",
+			   client);
+		mutex_unlock(&debugfs_mutex);
+		return 0;
+	}
+
 	mutex_lock(&client->lock);
 	for (n = rb_first(&client->handles); n; n = rb_next(n)) {
 		struct ion_handle *handle = rb_entry(n, struct ion_handle,
@@ -828,9 +864,11 @@ static int ion_debug_client_show(struct seq_file *s, void *unused)
 				atomic_read(&handle->ref.refcount),
 				handle->buffer);
 
+
 		seq_printf(s, "\n");
 	}
 	mutex_unlock(&client->lock);
+	mutex_unlock(&debugfs_mutex);
 	up_write(&ion_dev->lock);
 	return 0;
 }
@@ -957,6 +995,7 @@ void ion_client_destroy(struct ion_client *client)
 	struct rb_node *n;
 
 	pr_debug("%s: %d\n", __func__, __LINE__);
+	mutex_lock(&debugfs_mutex);
 	while ((n = rb_first(&client->handles))) {
 		struct ion_handle *handle = rb_entry(n, struct ion_handle,
 						     node);
@@ -976,6 +1015,7 @@ void ion_client_destroy(struct ion_client *client)
 	kfree(client->display_name);
 	kfree(client->name);
 	kfree(client);
+	mutex_unlock(&debugfs_mutex);
 }
 EXPORT_SYMBOL(ion_client_destroy);
 
@@ -1720,6 +1760,7 @@ static int ion_debug_heap_show(struct seq_file *s, void *unused)
 	seq_printf(s, "----------------------------------------------------\n");
 
 	down_read(&dev->lock);
+	mutex_lock(&debugfs_mutex);
 	for (n = rb_first(&dev->clients); n; n = rb_next(n)) {
 		struct ion_client *client = rb_entry(n, struct ion_client,
 						     node);
@@ -1738,6 +1779,8 @@ static int ion_debug_heap_show(struct seq_file *s, void *unused)
 		}
 	}
 	up_read(&dev->lock);
+	mutex_unlock(&debugfs_mutex);
+
 	seq_printf(s, "----------------------------------------------------\n");
 	seq_printf(s, "orphaned allocations (info is from last known client):"
 		   "\n");
@@ -1965,7 +2008,12 @@ debugfs_done:
 	init_rwsem(&idev->lock);
 	plist_head_init(&idev->heaps);
 	idev->clients = RB_ROOT;
+<<<<<<< HEAD
 	ion_dev = idev;
+=======
+	ion_root_client = &idev->clients;
+	mutex_init(&debugfs_mutex);
+>>>>>>> e5e4ee1de3c... BACKPORT: ion:synchronize debugfs callback and ion_client_destroy
 	return idev;
 }
 
