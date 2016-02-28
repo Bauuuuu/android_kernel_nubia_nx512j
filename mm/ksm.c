d9c7662acaabe1de36b60143e7068870801c6695
faust93 <monumentum@gmail.com>
2016-02-28 18:36:39 +0300

    UKSM added

--- mm/ksm.c
+++ mm/ksm.c
@@ -240,7 +240,7 @@ static int ksm_nr_node_ids = 1;
 #define KSM_RUN_MERGE	1
 #define KSM_RUN_UNMERGE	2
 #define KSM_RUN_OFFLINE	4
-static unsigned long ksm_run = KSM_RUN_MERGE;
+static unsigned long ksm_run = KSM_RUN_STOP;
 static void wait_while_offlining(void);
 
 static DECLARE_WAIT_QUEUE_HEAD(ksm_thread_wait);
@@ -2501,7 +2501,7 @@ static int __init ksm_init(void)
 		goto out_free;
 	}
 #else
-	ksm_run = KSM_RUN_MERGE;	/* no way for user to start it */
+	ksm_run = KSM_RUN_STOP;	/* no way for user to start it */
 
 #endif /* CONFIG_SYSFS */
 
