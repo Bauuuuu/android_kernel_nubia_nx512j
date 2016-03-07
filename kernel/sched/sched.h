69deb5888806cca0dfb3e59e8468d9fa4f56c4f1
faust93 <monumentum@gmail.com>
2016-03-07 14:04:27 +0300

    Implemented Fast-IDLING of CPU

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -380,6 +380,9 @@ struct root_domain {
 	cpumask_var_t span;
 	cpumask_var_t online;
 
+	/* Indicate more than one runnable task for any CPU */
+	bool overload;
+
 	/*
 	 * The "RT overload" flag: it gets set if a CPU has more than
 	 * one runnable RT task.
@@ -1369,14 +1372,18 @@ static inline void inc_nr_running(struct rq *rq)
 #endif
 	rq->nr_running++;
 
+if (rq->nr_running >= 2) {
+#ifdef CONFIG_SMP
+ if (!rq->rd->overload)
+ rq->rd->overload = true;
+#endif
+
 #ifdef CONFIG_NO_HZ_FULL
-	if (rq->nr_running == 2) {
 		if (tick_nohz_full_cpu(rq->cpu)) {
 			/* Order rq->nr_running write against the IPI */
 			smp_wmb();
 			smp_send_reschedule(rq->cpu);
 		}
-       }
 #endif
 #if defined(CONFIG_INTELLI_PLUG) || defined(CONFIG_HIMA_HOTPLUG)
 	write_seqcount_end(&nr_stats->ave_seqcnt);
