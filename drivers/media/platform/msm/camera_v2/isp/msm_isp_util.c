1ac42d2086b479a4ac41b70acf503382f254bb15
Bauuuuu <abansode@outlook.com>
2017-07-03 23:10:45 +0200

    msm: camera: remove epoch irq & camif support
    * old camera blobs and hence not compatible
    * issues: black screen on zoom or crashes camera
    
    Revert 7ca2574577ce8c4ed3c881bfc03709c8410bbb19.
    Revert f6275c1a64bdf67b4a3cdccecc6d4d6254e16938.
    Revert 6e026b42e0c10ca180b446fc0a1bfb3c9c216d55.
    Revert ac9d1fb94f8fa62f15a726e0981cee2bd45d8442.
    Revert 70dd43fab3fc00dc5e47767b81bbf879b1fba09d.
    
    Change-Id: I98496ef8fd0c3a79baea902154b86871d4e500f6

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
@@ -767,16 +767,6 @@ static long msm_isp_ioctl_unlocked(struct v4l2_subdev *sd,
 			start_fetch_eng(vfe_dev, arg);
 		mutex_unlock(&vfe_dev->core_mutex);
 		break;
-	case VIDIOC_MSM_ISP_REG_UPDATE_CMD:
-		if (arg) {
-			enum msm_vfe_input_src frame_src =
-				*((enum msm_vfe_input_src *)arg);
-			vfe_dev->hw_info->vfe_ops.core_ops.
-				reg_update(vfe_dev, (1 << frame_src));
-			vfe_dev->axi_data.src_info[frame_src].last_updt_frm_id =
-			  vfe_dev->axi_data.src_info[frame_src].frame_id;
-		}
-		break;
 	case VIDIOC_MSM_ISP_SET_SRC_STATE:
 		mutex_lock(&vfe_dev->core_mutex);
 		rc = msm_isp_set_src_state(vfe_dev, arg);
@@ -1130,9 +1120,11 @@ static int msm_isp_send_hw_cmd(struct vfe_device *vfe_dev,
 	case VFE_HW_UPDATE_LOCK: {
 		uint32_t update_id =
 			vfe_dev->axi_data.src_info[VFE_PIX_0].last_updt_frm_id;
-		if (update_id) {
-			ISP_DBG("%s hw update lock failed cur id %u, last id %u\n",
+		if (vfe_dev->axi_data.src_info[VFE_PIX_0].frame_id != *cfg_data
+			|| update_id == *cfg_data) {
+			ISP_DBG("%s hw update lock failed acq %d, cur id %u, last id %u\n",
 				__func__,
+				*cfg_data,
 				vfe_dev->axi_data.src_info[VFE_PIX_0].frame_id,
 				update_id);
 			return -EINVAL;
@@ -1756,8 +1748,6 @@ void msm_isp_do_tasklet(unsigned long data)
 		irq_ops->process_stats_irq(vfe_dev,
 			irq_status0, irq_status1, &ts);
 		irq_ops->process_reg_update(vfe_dev,
-			irq_status0, irq_status1, &ts);
-		irq_ops->process_epoch_irq(vfe_dev,
 			irq_status0, irq_status1, &ts);
 	}
 }
